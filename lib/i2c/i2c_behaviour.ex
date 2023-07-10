03401187c444b83a52a82200b5e4e4e03916efe5
Paul Wilson <paulanthonywilson@gmail.com>
2023-07-10 11:12:02 +0100

    More callbacks for functions with optional values

--- lib/i2c/i2c_behaviour.ex
+++ lib/i2c/i2c_behaviour.ex
@@ -3,7 +3,7 @@ defmodule Circuits.I2C.I2cBehaviour do
   A behaviour that adds a testing seam to `Circuits.I2C` to make developing a little bit easier to test.
 
   While the 2.0 version does come with the ability to swap in different back-ends, using a behaviour is a simpler way
-  of testing, eg using [Mox](https://hexdocs.pm/mox/).
+  of testing, for example by using [Mox](https://hexdocs.pm/mox/).
   """
 
   @type address() :: 0..127
@@ -21,8 +21,10 @@ defmodule Circuits.I2C.I2cBehaviour do
   @callback read!(bus(), address(), pos_integer(), [opt()]) :: binary()
 
   @callback write(bus(), address(), iodata(), [opt()]) :: :ok | {:error, term()}
+  @callback write(bus(), address(), iodata()) :: :ok | {:error, term()}
 
   @callback write!(bus(), address(), iodata(), [opt()]) :: :ok
+  @callback write!(bus(), address(), iodata()) :: :ok
 
   @callback write_read(bus(), address(), iodata(), pos_integer(), [opt()]) ::
               {:ok, binary()} | {:error, term()}
@@ -30,18 +32,25 @@ defmodule Circuits.I2C.I2cBehaviour do
               {:ok, binary()} | {:error, term()}
 
   @callback write_read!(bus(), address(), iodata(), pos_integer(), [opt()]) :: binary()
+  @callback write_read!(bus(), address(), iodata(), pos_integer()) :: binary()
+
   @callback close(bus()) :: :ok
+
   @callback bus_names() :: [binary()]
 
   @callback detect_devices(bus() | binary()) :: [address()] | {:error, term()}
-
   @callback detect_devices() :: :"do not show this result in output"
+
   @callback discover([address()], present?()) :: [{binary(), address()}]
+  @callback discover([address()]) :: [{binary(), address()}]
 
   @callback discover_one([address()], present?()) ::
               {:ok, {binary(), address()}} | {:error, :not_found | :multiple_possible_matches}
+  @callback discover_one([address()]) ::
+              {:ok, {binary(), address()}} | {:error, :not_found | :multiple_possible_matches}
 
   @callback discover_one!([address()], present?()) :: {binary(), address()}
+  @callback discover_one!([address()]) :: {binary(), address()}
 
   @callback device_present?(bus(), address()) :: boolean()
   @callback info() :: map()
